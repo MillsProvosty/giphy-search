003cb3cf531bd264ebf54dc37d56084955a3d02c
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-07-08 07:27:15 -0600

    First draft of test

--- spec/user_search_spec.rb
+++ spec/user_search_spec.rb
@@ -5,5 +5,18 @@ RSpec.describe "As a user on my dashboard page" do
     scenario "I shoulid see '5 Results', an id, url and gif "
     visit "/"
 
+    expect(page).to have_button("Search")
+
+    fill_in :search, with: "tiger"
+    click_button 'Search'
+
+    expect(current_path).to eq("/q=tiger")
+
+    expect(page).to have_content("5 Results")
+
+    within(page.all(".giphy_search_results")[0]) do
+      expect(page).to have_xpath("//img[contains(@src,'something.jpg')]")
+    end
+
   end
 end
