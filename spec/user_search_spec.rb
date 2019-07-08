0815dd425d6ad4e03d2aca11cf436d5518b8ad2a
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-07-08 07:13:09 -0600

    Adds faraday, pry and figaro to gemfile

---
+++ spec/user_search_spec.rb
@@ -0,0 +1,10 @@
+# require 'rails_helper'
+#
+# RSpec.describe "As a user "
+# # As a user
+# # When I visit "/"
+# # And I enter "tiger" in the search box
+# # and I click "Search"
+# # Then my path should be /search with "q=tiger" in the parameters
+# # And I should see "5 Results"
+# # And I should see an id, a url, and the gif.
